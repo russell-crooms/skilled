ff4c027b8878e0cd05963457d9227d52fb4795dd
Russell Crooms <russella.crooms@gmail.com>
2024-06-08 13:58:31 -0500

    Added styling to our cards.

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import icon_animation from "../../assets/icon-animation.svg";
+import icon_business from "../../assets/icon-business.svg";
+import icon_crypto from "../../assets/icon-crypto.svg";
+import icon_design from "../../assets/icon-design.svg";
+import icon_photography from "../../assets/icon-photography.svg";
+import './Cards.css';
 
 const Cards = () => {
   return (
-    <div>Cards</div>
-  )
-}
+    <div className="card-container">
+      <div className="cards">
+        <div className="colorful-card">
+          <h2>Check out our most popular courses!</h2>
+        </div>
+        <div className="white-card">
+          <img src={icon_animation} alt="" />
+          <h2>Animation</h2>
+          <p>
+            Learn the latest animation techniques to create stunning motion
+            design and captivate your audience.
+          </p>
+          <p className="cards-get-started row-one">Get Started</p>
+        </div>
+        <div className="white-card">
+          <img src={icon_design} alt="" />
+          <h2>Design</h2>
+          <p>
+            Create beautiful, usable interfaces to help shape the future of how
+            the web looks.
+          </p>
+          <p className="cards-get-started">Get Started</p>
+        </div>
+        <div className="white-card">
+          <img src={icon_photography} alt="" />
+          <h2>Photography</h2>
+          <p>
+            Explore critical fundamentals like lighting, composition, and focus
+            to capture exceptional photos.
+          </p>
+          <p className="cards-get-started">Get Started</p>
+        </div>
+        <div className="white-card design">
+          <img src={icon_crypto} alt="" />
+          <h2>Crypto</h2>
+          <p>
+            All you need to know to get started investing in crypto. Go from
+            beginner to advanced with this 54 hour course.
+          </p>
+          <p className="cards-get-started">Get Started</p>
+        </div>
+        <div className="white-card">
+          <img src={icon_business} alt="" />
+          <h2>Business</h2>
+          <p>
+            A step-by-step playbook to help you start, scale and sustain your
+            business without outside investment.
+          </p>
+          <p className="cards-get-started">Get Started</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
