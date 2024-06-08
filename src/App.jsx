1ee2d32ec7d40bbb75862388b70714de9b8a27a4
Russell Crooms <russella.crooms@gmail.com>
2024-06-08 12:06:15 -0500

    Created the Navbar, Hero, and Cards components, and mounted them to the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Hero from './components/hero/Hero'
+import NavBar from './components/navBar/NavBar'
+import Cards from './components/cards/Cards'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+    <NavBar />
+    <Hero />
+    <Cards />
+  </div>
   )
 }
 
