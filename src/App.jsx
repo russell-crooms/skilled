d7d90d968edb3bff6a3496b3597c06fb86b44794
Russell Crooms <russella.crooms@gmail.com>
2024-06-09 23:41:50 -0500

    Created the footer component, and mounted this to the App.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/hero/Hero'
 import NavBar from './components/navBar/NavBar'
 import Cards from './components/cards/Cards'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <NavBar />
     <Hero />
     <Cards />
+    <Footer />
   </div>
   )
 }
