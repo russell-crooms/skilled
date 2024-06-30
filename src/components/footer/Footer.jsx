20793517e4e9285d9df70357942915d93f5160cd
Russell Crooms <russella.crooms@gmail.com>
2024-06-29 19:06:32 -0500

    Fixed dimensions on desktop devices.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,17 +1,19 @@
-import './Footer.css';
-import logo_light from '../../assets/logo-light.svg';
+import "./Footer.css";
+import logo_light from "../../assets/logo-light.svg";
 
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="footer-items">
         <div className="footer-logo">
-            <img src={logo_light} />
+          <img src={logo_light} />
         </div>
         <div className="footer-button">
-            <p>Get Started</p>
+          <p>Get Started</p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
