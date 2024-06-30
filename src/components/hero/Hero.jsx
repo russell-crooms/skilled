b0b390f6363a786ebf32beb0d22947042dc6b70a
Russell Crooms <russella.crooms@gmail.com>
2024-06-30 01:56:07 -0500

    Added mobile image, but somehow it has vanished.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,29 +1,35 @@
 import "./Hero.css";
 import hero_image_desktop from "../../assets/image-hero-desktop.png";
 import hero_image_tablet from "../../assets/image-hero-tablet.webp";
+import hero_image_mobile from "../../assets/image-hero-mobile.png";
 
 const Hero = () => {
   return (
     <div className="hero-container">
       <div className="hero-container-left">
         <div className="hero-header">
-          <h2>Maximize skill, <br></br>minimize budget</h2>
+          <h2>
+            Maximize skill, <br></br>minimize budget
+          </h2>
         </div>
         <div className="hero-para">
           <p>
-            Our modern courses across a range of in-demand<br></br>skills will give you
-            the knowledge you need to live<br></br>the life you want.
+            Our modern courses across a range of in-demand skills will give you
+            the knowledge you need to live the life you want.
           </p>
         </div>
         <div className="hero-button">
-            <button>Get Started</button>
+          <button>Get Started</button>
         </div>
       </div>
       <div className="hero-container-right">
         <img src={hero_image_desktop} alt="" />
       </div>
       <div className="hero-image-tablet">
-      <img src={hero_image_tablet} alt="" />
+        <img src={hero_image_tablet} alt="" />
+      </div>
+      <div className="hero-image-mobile">
+        <img src={hero_image_mobile} alt="" />
       </div>
     </div>
   );
