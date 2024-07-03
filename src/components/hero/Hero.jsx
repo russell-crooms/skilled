ecfda1439ebc9ee62a31e195300b0ebb1838e4b6
Russell Crooms <russella.crooms@gmail.com>
2024-07-02 22:26:57 -0500

    Cleaned up the project header.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,14 +5,14 @@ import hero_image_mobile from "../../assets/image-hero-mobile.png";
 
 const Hero = () => {
   return (
-    <div className="hero-container">
-      <div className="hero-container-left">
-        <div className="hero-header">
+    <div className="hero-section">
+      <div className="hero-section-left">
+        <div className="hero-header-text">
           <h2>
             Maximize skill, <br></br>minimize budget
           </h2>
         </div>
-        <div className="hero-para">
+        <div className="hero-paragraph">
           <p>
             Our modern courses across a range of in-demand skills will give you
             the knowledge you need to live the life you want.
@@ -22,11 +22,11 @@ const Hero = () => {
           <button>Get Started</button>
         </div>
       </div>
-      <div className="hero-container-right">
+      <div className="hero-image">
         <div className="hero-image-desktop">
           <img src={hero_image_desktop} alt="" />
         </div>
-        <div className="hero-container-tablet">
+        <div className="hero-image-tablet">
           <img src={hero_image_tablet} alt="" />
         </div>
         <div className="hero-image-mobile">
